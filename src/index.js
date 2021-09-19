944cea73c921c6c37f33d904590052e1d1f481d9
Siddhartha Rana <rana.siddharth994@gmail.com>
2021-09-19 12:53:05 +0530

    Header&Footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css'; // import custom css after bootstrap css so it can be overriden
 import App from './App';
 import reportWebVitals from './reportWebVitals';
