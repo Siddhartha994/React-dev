378efb83847003e15847714e390b21bd32d33ff6
Siddhartha Rana <rana.siddharth994@gmail.com>
2021-10-02 19:53:50 +0530

    Uncontrolled Form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron,
+        Button,Modal,ModalBody, ModalHeader, FormGroup, Label, Input,Form} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -7,8 +8,11 @@ class Header extends Component {
         super(props);
     
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal= this.toggleModal.bind(this);
+        this.handleLogin= this.handleLogin.bind(this);
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         };
     }
 
@@ -17,7 +21,16 @@ class Header extends Component {
             isNavOpen: !this.state.isNavOpen
             });
         }
-
+        toggleModal(){
+            this.setState({
+                isModalOpen: !this.state.isModalOpen
+            });
+        }
+        handleLogin(event){
+            this.toggleModal();
+            alert("username: " + this.username.value + "Password: " + this.password.value + "Remember: " + this.remember.checked);
+            event.preventDefault();
+        }
     render() {
         return(
             <div>
@@ -40,6 +53,14 @@ class Header extends Component {
                                 <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign-in fa-lg"></span>
+                                        Login
+                                    </Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -53,6 +74,32 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle = {this.toggleModal}>
+                    <ModalHeader toggle = {this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id = "username" name ="username"
+                                innerRef={(input) => this.username = input} />    {/* Retrieve info from input field */}
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="username">Password</Label>
+                                <Input type="text" id = "username" name ="username" 
+                                innerRef={(input) => this.password = input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name= "remember "
+                                    innerRef={(input) => this.remember= input} />
+                                    Remember Me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" className="bg-primary">Login</Button>
+                        </Form>   
+
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
