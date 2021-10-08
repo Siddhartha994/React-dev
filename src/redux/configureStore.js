efcbda284cb5df0d94958ae1e0ca663a876a8259
Siddhartha Rana <rana.siddharth994@gmail.com>
2021-10-08 22:42:51 +0530

    Splitting(partial state)& then Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,20 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from "./reducer";
+import { createStore,combineReducers } from "redux";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Promotions } from "./promotions";
+import { Leaders } from "./leaders";
+
+// *from prev. commit: remove initialState since every individual file storing its state
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        // Recompose reducer functions
+        combineReducers({       // reducers: ReducersMapObject         maps simpler reducer functions into properties
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
     return store;
 }
