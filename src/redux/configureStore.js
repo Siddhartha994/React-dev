b6626eda6e9b92309d282516afc34f3a711a872b
Siddhartha Rana <rana.siddharth994@gmail.com>
2021-10-11 22:11:22 +0530

    React-Redux-Form Revisited/ Persistence enabled

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore,combineReducers,applyMiddleware } from "redux";
+import { createForms } from "react-redux-form";
 import { Dishes } from "./dishes";
 import { Comments } from "./comments";
 import { Promotions } from "./promotions";
 import { Leaders } from "./leaders";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-
+import { InitialFeedback } from "./forms";
 
 
 export const ConfigureStore = () => {
@@ -15,7 +16,12 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback   
+            })
+            /*once we import the initial feedback and initialize it like this, 
+            it'll add  necessary REDUCER functions and  STATE info in my CREATE STORE*/
         }),
         applyMiddleware(thunk,logger)
     );
