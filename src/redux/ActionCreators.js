bce041b66851c06cd98bb3a4b64dad899f17e2e5
Siddhartha Rana <rana.siddharth994@gmail.com>
2021-10-16 14:09:58 +0530

    fetch handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -16,8 +16,26 @@ export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
     // actual communication with server
     return fetch(baseUrl + 'dishes')    //providing full location of url
+        //promise handler
+        .then(response => { 
+            if (response.ok){
+                return response;
+            }
+            else{ 
+                var error = new error('Error'+ response.status + ':' + response.statusText);
+                error.response = response; 
+                throw error;    // when throwing error in a promise handling, you can catch it
+            }
+        },
+        // error handler(part of promise handler)
+        error => {  
+            var errmess = new Error(error.message);
+            throw errmess; 
+        }
+        )
         .then(response => response.json())
-        .then(dishes => dispatch(addDishes(dishes)));
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)));  //catches error bcoz of else or the 
 }
 
 export const dishesLoading = () => ({
@@ -38,8 +56,26 @@ export const addDishes = (dishes) => ({
 
 export const fetchComments = () => (dispatch) => {    
     return fetch(baseUrl + 'comments')
+    .then(response => { 
+        if (response.ok){
+            return response;
+        }
+        else{ 
+            var error = new error('Error'+ response.status + ':' + response.statusText);
+            error.response = response; 
+            throw error;    // when throwing error in a promise handling, you can catch it
+        }
+    },
+    // error handler(part of promise handler)
+    error => {  
+        var errmess = new Error(error.message);
+        throw errmess; 
+    }
+    )
     .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));  //catches error bcoz of else or the 
+
 };
 
 export const commentsFailed = (errmess) => ({
@@ -59,8 +95,25 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading());
 
     return fetch(baseUrl + 'promotions')
+        .then(response => { 
+            if (response.ok){
+                return response;
+            }
+            else{ 
+                var error = new error('Error'+ response.status + ':' + response.statusText);
+                error.response = response; 
+                throw error;    // when throwing error in a promise handling, you can catch it
+            }
+        },
+        // error handler(part of promise handler)
+        error => {  
+            var errmess = new Error(error.message);
+            throw errmess; 
+        })
     .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)));
+    .then(promos => dispatch(addPromos(promos)))
+    .catch(error => dispatch(promosFailed(error.message)));  //catches error bcoz of else or the 
+
 }
 
 export const promosLoading = () => ({
